7950013696095f235f6edaecde8502957c1e108b
Lucas Suarez <lucas.suarez.dev@gmail.com>
2022-11-13 17:04:59 -0400

    Initial Commit

---
+++ myApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Lucas Suarez Blanco on 11/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
